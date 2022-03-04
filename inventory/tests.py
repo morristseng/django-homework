4effc4bb900c8cf4813d77458548990c3efafcf8
Morris Tseng <morris.tseng@gofreight.co>
2022-03-04 14:41:54 +0800

    feat(django-homework-assignment2): Add inventory status view and related test cases.
    [why]
    As for assignment2 requirement (實作另一個 List View 頁面，可觀看目前所有的 Shipping / Receiving 加總的結果)
    
    [how]
    Using QuerySet's aggregate to get total quantity.
    Also wrote 3 tests to cover both simple and complex cases.

--- inventory/tests.py
+++ inventory/tests.py
@@ -1,3 +1,46 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Commodity, Inventory, TradeParner
+
+
+def create_basic():
+    parner = TradeParner.objects.create(name='parner1', address='add1')
+    commodity1 = Commodity.objects.create(name='Switch', description='Switch OLED', customer=parner)
+    commodity2 = Commodity.objects.create(name='PS5', description='PS5', customer=parner)
+    return parner, commodity1, commodity2
+
+class ListInventoryStatusTests(TestCase):
+    def test_receiving_quantity(self):
+        parner, commodity, _ = create_basic()
+        Inventory.objects.create(customer=parner, inventory_type='receiving', commodity=commodity, quantity=50)
+        Inventory.objects.create(customer=parner, inventory_type='receiving', commodity=commodity, quantity=4)
+        response = self.client.get(reverse('status'))
+        object = response.json()
+        self.assertEqual(object["count"], 1)
+        self.assertEqual(object["results"][0]["quantity"], 54)
+
+    def test_shipping_quantity(self):
+        parner, commodity, _ = create_basic()
+        Inventory.objects.create(customer=parner, inventory_type='shipping', commodity=commodity, quantity=70)
+        Inventory.objects.create(customer=parner, inventory_type='shipping', commodity=commodity, quantity=3)
+        response = self.client.get(reverse('status'))
+        object = response.json()
+        self.assertEqual(object["count"], 1)
+        self.assertEqual(object["results"][0]["quantity"], -73)
+
+    def test_multiple_commodity(self):
+        parner, commodity1, commodity2 = create_basic()
+        Inventory.objects.create(customer=parner, inventory_type='shipping', commodity=commodity1, quantity=70)
+        Inventory.objects.create(customer=parner, inventory_type='shipping', commodity=commodity1, quantity=3)
+        Inventory.objects.create(customer=parner, inventory_type='receiving', commodity=commodity1, quantity=50)
+        Inventory.objects.create(customer=parner, inventory_type='receiving', commodity=commodity1, quantity=4)
+        Inventory.objects.create(customer=parner, inventory_type='shipping', commodity=commodity2, quantity=170)
+        Inventory.objects.create(customer=parner, inventory_type='shipping', commodity=commodity2, quantity=13)
+        Inventory.objects.create(customer=parner, inventory_type='receiving', commodity=commodity2, quantity=150)
+        Inventory.objects.create(customer=parner, inventory_type='receiving', commodity=commodity2, quantity=14)
+        response = self.client.get(reverse('status'))
+        object = response.json()
+        self.assertEqual(object["count"], 2)
+        self.assertEqual(object["results"][0]["quantity"], -19)
+        self.assertEqual(object["results"][1]["quantity"], -19)
--- inventory/views.py
+++ inventory/views.py
@@ -1,27 +1,52 @@
 from django.core.paginator import Paginator
+from django.db.models import Q, Sum
+from django.db.models.functions import Coalesce
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Inventory
-from .serializers import InventorySerializer
+from .serializers import InventorySerializer, InventoryStatusSerializer
 
 
 def ListInventoryView(request):
     return render(request, 'inventory/index.html')
 
+def ListInventoryStatusView(request):
+    return render(request, 'inventory/status.html')
+
+def paginatorHelper(request, queryset):
+    page_num = int(request.GET.get('page', '1'))
+    page_size = int(request.GET.get('o', '50'))
+    # Question:
+    # Why not just use pagination from django rest framework?
+    # https://www.django-rest-framework.org/api-guide/pagination/
+    paginator = Paginator(queryset, page_size)
+    return paginator.page(page_num), paginator.count
+
+
 class ListInventory(APIView):
     def get(self, request):
-        page = int(request.GET.get('page', '1'))
-        offset = int(request.GET.get('o', '50'))
-        queryset = Inventory.objects.all()
-        # Question:
-        # Why not just use pagination from django rest framework?
-        # https://www.django-rest-framework.org/api-guide/pagination/
-        p = Paginator(queryset, offset)
-        cur_page = p.page(page)
+        queryset = Inventory.objects.select_related('customer', 'commodity').all().order_by('id')
+        cur_page, count = paginatorHelper(request, queryset)
         serializer = InventorySerializer(cur_page.object_list, many=True)
         return Response({
-            "count": p.count,
+            "count": count,
+            "results": serializer.data,
+        })
+
+class ListInventoryStatus(APIView):
+    def get(self, request):
+        receiving_quantity = Coalesce(Sum('quantity', filter=Q(inventory_type='receiving')), 0)
+        shipping_quantity = Coalesce(Sum('quantity', filter=Q(inventory_type='shipping')), 0)
+        queryset = Inventory.objects \
+            .values('commodity') \
+            .order_by('commodity') \
+            .annotate(quantity=receiving_quantity-shipping_quantity) \
+            .values('commodity__name', 'commodity__description', 'quantity')
+        cur_page, count = paginatorHelper(request, queryset)
+        serializer = InventoryStatusSerializer(cur_page.object_list, many=True)
+        return Response({
+            "count": count,
             "results": serializer.data,
         })
